e37a422629187626d563a14e0f9a82c98a5e9430
zzihyeto <ljh9055@gmail.com>
2025-07-17 00:26:31 +0900

    launch 8000으로 수정

--- src/components/greeting.jsx
+++ src/components/greeting.jsx
@@ -74,7 +74,7 @@ const Greeting = () => {
         앞날을 축복해 주시면 감사하겠습니다.
       </Content>
       <GroomBride data-aos="fade-up">
-        {GROOM_FATHER_NAME} · {GROOM_MOTHER_NAME}의 장남 {GROOM_NAME}
+        {GROOM_FATHER_NAME} · {GROOM_MOTHER_NAME}의 차남 {GROOM_NAME}
         <br />
         {BRIDE_FATHER_NAME} · {BRIDE_MOTHER_NAME}의 장녀 {BRIDE_NAME}
       </GroomBride>
