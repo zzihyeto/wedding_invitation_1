cc2464dbcc9b19eaa67c3d5de883a49fa328e0c5
zzihyeto <ljh9055@gmail.com>
2025-07-16 23:58:59 +0900

    config , location 수정해보기

--- config.js
+++ config.js
@@ -3,19 +3,19 @@ export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "1970년 01월 01일, 목요일 오전 12시 00분";
-export const WEDDING_LOCATION = "○○○웨딩, ○층 ○○홀";
+export const WEDDING_DATE = "2026년 01월 31일, 토요일 오후 12시 00분";
+export const WEDDING_LOCATION = "성남 혜성 교회";
 
-export const GROOM_NAME = "○○○";
+export const GROOM_NAME = "임찬교";
 export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const GROOM_FATHER_NAME = "○○○";
+export const GROOM_FATHER_NAME = "임희종";
 export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const GROOM_MOTHER_NAME = "○○○";
+export const GROOM_MOTHER_NAME = "고수기";
 export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 
-export const BRIDE_NAME = "○○○";
-export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const BRIDE_FATHER_NAME = "○○○";
+export const BRIDE_NAME = "이지혜";
+export const BRIDE_ACCOUNT_NUMBER = "카카오뱅크 3333-16-0948873";
+export const BRIDE_FATHER_NAME = "이찬욱";
 export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const BRIDE_MOTHER_NAME = "○○○";
+export const BRIDE_MOTHER_NAME = "김정임";
 export const BRIDE_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
--- src/components/location.jsx
+++ src/components/location.jsx
@@ -47,8 +47,8 @@ const Location = () => {
   const executeScript = () => {
     const scriptTag = document.createElement("script");
     const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-    "timestamp" : "1652464367301",
-    "key" : "2a8fe",
+    "timestamp" : "1752676913716",
+    "key" : "5qqft64ust7",
     "mapWidth" : "640",
     "mapHeight" : "360"
   }).render();`);
